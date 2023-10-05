f2248e4f9647e935aa52b32dcb8deb3465cefc0f
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-05 15:05:15 -0500

    working on navtabs

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import NavTabs from 'react';
 
-//import { Link } from 'react-dom';
+import { Link } from 'react-dom';
 
-// function NavTabs() {
-//     return (
-//       <Nav>
-//         <ul>
-//           <li>
-//             <Link to="/">About</Link>
-//           </li>
-//     <li>
-//       <Link to="/contact">Contact</Link>
-//     </li>
-//     <li>
-//       <Link to="/portfolio">Portfolio</Link>
-//     </li>
-//     <li>
-//       <Link><a href="/components/Resume">Resume</a></Link>
-//     </li>
+ function NavTabs() {
+  return (
+      <Nav>
+        <ul>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+    <li>
+      <Link to="/contact">Contact</Link>
+    </li>
+    <li>
+      <Link to="/portfolio">Portfolio</Link>
+    </li>
+    <li>
+      <Link><a href="/components/Resume">Resume</a></Link>
+    </li>
       
      
-//       </ul>
-//     </Nav>
-//       );
-// }
+      </ul>
+    </Nav>
+      );
+}
 
  export default NavTabs;
