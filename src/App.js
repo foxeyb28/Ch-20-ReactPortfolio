9b875a1b5ac96f9c4ad2bb0f14d92287bb7418f4
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-05 18:19:09 -0500

    fixed errors in app.js

--- src/App.js
+++ src/App.js
@@ -2,32 +2,23 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
-//import NavTabs from './components/NavTabs';
-import { Routes, Route } from 'react-router-dom';
+import NavTabs from './components/NavTabs';
+
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import Resume from './components/pages/Resume';
 import Portfolio from './components/pages/Portfolio';
 
-function App() {
-  return (
+const App = () => 
 
-    <div>
-      <Header/>
-      <Routes>
+      <Portfolio />;
+      <About />;
+      <Contact />;
+      <Resume />;
+      <Header />;
+      <Footer />;
+      <NavTabs />;
+ 
 
-      <Route path='/' element={<Portfolio/>}/>
-      <Route path='/' element={<About/>}/>
-      <Route path='/' element={<Contact/>}/>
-      <Route path='/' element={<Resume/>}/>
-      </Routes>
-<Footer/>
-
-
-     
-      <p>hello</p>
-    </div>
-  );
-}
 
 export default App;
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,13 +2,13 @@ import React  from 'react';
 import imageOfAlicia from '../../assets/alicia.jpeg';
 import ShoutOut from '../../assets/shout out.png';
 import MovieReviewScreenShot from '../../assets/movie review screen shot.png';
-import { Route } from 'react-router-dom';
+
 
 export default function Portfolio() {
     
    
    return (
-     <Route>
+     
 
     <div>
       <h1>Portfolio</h1>
@@ -21,7 +21,7 @@ export default function Portfolio() {
 
 
    </div>
-     </Route>
+     
   
    )
 };
