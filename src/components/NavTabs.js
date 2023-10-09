b7e3be11112ea6b31810a21490272411906f4d01
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-10-09 13:54:24 -0500

    fixed navtabs error

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,28 +1,53 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import Header from './Header';
+ import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
- function NavTabs() {
+function NavTabs({currentPage, handlePageChange}) {
+
   return (
-             <div>
-              <Header/>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-    <li>
-      <Link to="/contact">Contact</Link>
-    </li>
-    <li>
-      <Link to="/portfolio">Portfolio</Link>
-    </li>
-    <li>
-      <Link to="/Resume">Resume</Link>
-    </li>
-      
-     
-      </div>
     
-      );
-}
+    <ul className="nav nav-tabs">
+    <li className="nav-item">
+      <a
+        href="#About"
+        onClick={() => handlePageChange('About')}
+        className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#Contact"
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#Portfolio"
+            onClick={() => handlePageChange('Portfolio')}
+            className={currentPage === 'BPortfolio' ? 'nav-link active' : 'nav-link'}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#Resume"
+              onClick={() => handlePageChange('Resume')}
+              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        );
+      }
+
 
  export default NavTabs;
